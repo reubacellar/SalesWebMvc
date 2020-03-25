2a93d782654e52dd181f8c8e2ceeef42a1ea7aab
Reuel Bacellar <reubacellar@gmail.com>
2020-03-25 14:09:10 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -8,7 +8,7 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
+        public string Email { get; set; }       
         public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
